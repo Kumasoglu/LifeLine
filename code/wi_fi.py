996b154b6aa4328589a9a1024f2a409fa04e2780
MuhammedAliKumasoglu <75090388+Kumasoglu@users.noreply.github.com>
2024-01-26 19:57:08 +0300

    Update wi_fi.py

--- code/wi_fi.py
+++ code/wi_fi.py
@@ -40,11 +40,10 @@ class OTAUpdater:
 
     def update_file(self, url, dest):
         try:
-            response = urequests.get(url, stream=True)
+            response = urequests.get(url)
             if response.status_code == 200:
                 with open(dest, 'w') as f:
-                    for chunk in response.iter_content(1024):  # Read in chunks of 1KB
-                        f.write(chunk)
+                    f.write(response.text)
                 response.close()
                 print(f"Updated {dest}")
             else:
