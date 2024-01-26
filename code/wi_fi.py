6d27a61369a254d9442f3647b5bc611647efa1c5
MuhammedAliKumasoglu <75090388+Kumasoglu@users.noreply.github.com>
2024-01-26 20:50:04 +0300

    Update wi_fi.py

--- code/wi_fi.py
+++ code/wi_fi.py
@@ -1,7 +1,7 @@
 import network
 import urequests
 import time
-import gc
+import uhashlib
 
 class WiFiConnection:
     def __init__(self, ssid, password, attempts=2, timeout=10):
@@ -32,30 +32,93 @@ class WiFiConnection:
 
         print("Failed to connect to Wi-Fi after multiple attempts")
         return False
-    
 
-class OTAUpdater:
-    def __init__(self, base_url, file_list):
-        self.base_url = base_url
-        self.file_list = file_list
+class Senko:
+    raw = "https://raw.githubusercontent.com"
+    github = "https://github.com"
 
-    def update_file(self, url, dest):
-        try:
-            response = urequests.get(url)
-            if response.status_code == 200:
-                with open(dest, 'w') as f:
-                    f.write(response.text)
-                response.close()
-                print(f"Updated {dest}")
-            else:
-                print(f"Failed to download {url}")
-        except Exception as e:
-            print(f"Error updating {dest}: {e}")
-        gc.collect()  # Trigger garbage collection after updating each file
+    def __init__(self, user, repo, url=None, branch="main", working_dir="code", files=["boot.py", "main.py", "circullarBuffer.py", "ecg_filters.py", "heartRate.py", "wi_fi.py", "backup.py"], headers={}):
+        """Senko OTA agent class.
+
+        Args:
+            user (str): GitHub user.
+            repo (str): GitHub repo to fetch.
+            branch (str): GitHub repo branch. (master)
+            working_dir (str): Directory inside GitHub repo where the micropython app is.
+            url (str): URL to root directory.
+            files (list): Files included in OTA update.
+            headers (list, optional): Headers for urequests.
+        """
+        self.base_url = "{}/{}/{}".format(self.raw, user, repo) if user else url.replace(self.github, self.raw)
+        self.url = url if url is not None else "{}/{}/{}".format(self.base_url, branch, working_dir)
+        self.headers = headers
+        self.files = files
+
+    def _check_hash(self, x, y):
+        x_hash = uhashlib.sha1(x.encode())
+        y_hash = uhashlib.sha1(y.encode())
+
+        x = x_hash.digest()
+        y = y_hash.digest()
+
+        if str(x) == str(y):
+            return True
+        else:
+            return False
+
+    def _get_file(self, url):
+        payload = urequests.get(url, headers=self.headers)
+        code = payload.status_code
+
+        if code == 200:
+            return payload.text
+        else:
+            return None
+
+    def _check_all(self):
+        changes = []
+
+        for file in self.files:
+            latest_version = self._get_file(self.url + "/" + file)
+            if latest_version is None:
+                continue
+
+            try:
+                with open(file, "r") as local_file:
+                    local_version = local_file.read()
+            except:
+                local_version = ""
+
+            if not self._check_hash(latest_version, local_version):
+                changes.append(file)
+
+        return changes
+
+    def fetch(self):
+        """Check if newer version is available.
+
+        Returns:
+            True - if is, False - if not.
+        """
+        if not self._check_all():
+            return False
+        else:
+            return True
 
     def update(self):
-        for file in self.file_list:
-            url = f"{self.base_url}/{file}"
-            self.update_file(url, file)
-        print("All updates complete.")
+        """Replace all changed files with newer one.
+
+        Returns:
+            True - if changes were made, False - if not.
+        """
+        changes = self._check_all()
+
+        for file in changes:
+            with open(file, "w") as local_file:
+                local_file.write(self._get_file(self.url + "/" + file))
+
+        if changes:
+            return True
+        else:
+            return False
 
